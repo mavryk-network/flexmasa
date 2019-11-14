9baab66aab086307405c5168d3a8ad03837bc742
Sebastien Mondet <seb@mondet.org>
2019-11-14 15:24:14 -0500

    Add documentation to `Hard-fork.mli`

--- src/lib/hard_fork.mli
+++ src/lib/hard_fork.mli
@@ -1,6 +1,17 @@
 open Internal_pervasives
 
-type t =
+(** User-activated Upgrades, a.k.a. Hard Forks *)
+
+(**
+
+This module provides helpers to make “hard-fork” protocol changes happen
+in sandboxes.
+    
+See ["src/lib/interactive_mini_network.ml"] for an example of usage.
+
+*)
+
+type t = private
   { level: int
   ; protocol_hash: string
   ; name: string
@@ -14,9 +25,17 @@ val cmdliner_term :
   -> ?prefix:string
   -> unit
   -> t option Cmdliner.Term.t
+(** For a given [prefix] (default ["hard-fork"]), provide a family
+    of ["--<prefix>*"] command-line options allowing to configure a
+    user-activated-upgrade. *)
 
 val executables : t -> Tezos_executable.t list
+(** Get all the protocol-specific executable definitions (the “daemons”) involved in
+    this hard-fork. *)
+
 val node_network_config : t -> string * [> Ezjsonm.t]
+(** Generate the JSON field for the ["{ network : { ... } }"] part of a
+    node's configuration file. *)
 
 val keyed_daemons :
      t
@@ -24,3 +43,4 @@ val keyed_daemons :
   -> key:string
   -> node:Tezos_node.t
   -> Tezos_daemon.t list
+(** Prepare the baker and endorse daemons to secure the hard-fork. *)
