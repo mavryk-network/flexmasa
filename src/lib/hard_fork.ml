bdb04050277c052019af747195e01f20ac5c2ccd
Sebastien Mondet <seb@mondet.org>
2021-11-16 14:09:24 -0500

    Simplify `--hard-fork` option
    - Get the canonical value for the protocol hash.
    - Use the protocol-kind to look for executables too.

--- src/lib/hard_fork.ml
+++ src/lib/hard_fork.ml
@@ -14,7 +14,12 @@ let cmdliner_term base_state ~docs ?(prefix = "hard-fork") () =
   let open Cmdliner.Term in
   let extra_doc = Fmt.str " for the hard-fork (requires --%s)" prefix in
   pure (fun hard_fork baker endorser accuser ->
-      Option.map hard_fork ~f:(fun (level, protocol_kind, protocol_hash) ->
+      Option.map hard_fork ~f:(fun (level, protocol_kind, protocol_hash_opt) ->
+          let protocol_hash =
+            match protocol_hash_opt with
+            | None | Some "" ->
+                Tezos_protocol.Protocol_kind.canonical_hash protocol_kind
+            | Some s -> s in
           { level
           ; name= prefix
           ; protocol_kind
@@ -28,10 +33,13 @@ let cmdliner_term base_state ~docs ?(prefix = "hard-fork") () =
            (some
               (t3 ~sep:':' int
                  (enum Tezos_protocol.Protocol_kind.names)
-                 string ) )
+                 (some string) ) )
            None
-           (info [prefix] ~doc:"Make a hard-fork happen" ~docs
-              ~docv:"LEVEL:PROTOCOL-KIND:PROTOCOL-HASH" ) ))
+           (info [prefix]
+              ~doc:
+                "Make a hard-fork happen (PROTOCOL-HASH is optional, the \
+                 default is the canonical one for the given protocol-kind)."
+              ~docs ~docv:"LEVEL:PROTOCOL-KIND:[PROTOCOL-HASH]" ) ))
   $ Tezos_executable.cli_term ~extra_doc base_state `Baker prefix
   $ Tezos_executable.cli_term ~extra_doc base_state `Endorser prefix
   $ Tezos_executable.cli_term ~extra_doc base_state `Accuser prefix
@@ -50,10 +58,7 @@ let node_network_config t =
           ; ("replacement_protocol", string t.protocol_hash) ] ] )
 
 let keyed_daemons t ~client ~key ~node =
-  let protocol_kind =
-    (* We assume that we are not going to need the protocol kind and that
-       the binaries have been provided in the CLI. *)
-    Tezos_protocol.Protocol_kind.default in
+  let protocol_kind = t.protocol_kind in
   [ Tezos_daemon.baker_of_node ~name_tag:t.name ~exec:t.baker ~client node ~key
       ~protocol_kind
   ; Tezos_daemon.endorser_of_node ~name_tag:t.name ~exec:t.endorser ~client
