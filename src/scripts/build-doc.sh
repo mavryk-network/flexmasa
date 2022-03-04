d7bc87d12cf93d1cb05f2d027c99dc3cb3d1f977
Phil Saxton <phillipsaxton@gmail.com>
2022-03-04 14:51:35 -0500

    Build-doc: Adds Daemons-upgrade.md to website.

--- src/scripts/build-doc.sh
+++ src/scripts/build-doc.sh
@@ -88,6 +88,22 @@ EOF
 } | omd >> "$mini_net_fragment"
 mini_net="$output_path/mini-net.html"
 
+daemons_upgrade_fragment=$(mktemp "/tmp/daemons-upgrade-XXXX.html")
+{
+    cat ./src/doc/daemons-upgrade.md ;
+    cat <<'EOF'
+
+## Manpage Of daemons-upgrade
+
+For convenience, here is the output of `flextesa daemons-upgrade --help`:
+
+EOF
+    echo '``````'
+    ./flextesa daemons-upgrade --help=plain
+    echo '``````'
+} | omd >> "$daemons_upgrade_fragment"
+daemons_upgrade="$output_path/daemons-upgrade.html"
+
 make_page () {
     input="$1"
     output="$2"
@@ -115,10 +131,11 @@ EOF
 make_page "$lib_index_fragment" "$lib_index" "Flextesa: API"
 make_page "$main_index_fragment" "$main_index" "Flextesa: Home"
 make_page "$mini_net_fragment" "$mini_net" "Flextesa: Mini-net Command"
-
+make_page "$daemons_upgrade_fragment" "$daemons_upgrade" "Flextesa: daemons-upgrade Command"
 
 say "done: file://$PWD/$main_index"
 say "done: file://$PWD/$mini_net"
+say "done: file://$PWD/$daemons_upgrade"
 say "done: file://$PWD/$lib_index"
 say "done: file://$PWD/$output_path/flextesa/Flextesa/index.html"
 
