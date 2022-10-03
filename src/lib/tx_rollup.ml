29a34aada13232e3d999db717675e84cfb854d77
Phil Saxton <phillipsaxton@gmail.com>
2022-10-03 14:13:48 -0400

    tx_rollup: Add node start scripts and mli file.
    Basically a complete rewrite of the module.
    
    On branch psaxton-flextesa-tx-rollup-sandbox
    Your branch and 'origin/main' have diverged,
    and have 3 and 112 different commits each, respectively.
      (use "git pull" to merge the remote branch into yours)
    
    Changes to be committed:
    	modified:   flextesa/src/lib/tx_rollup.ml
    	modified:   flextesa/src/lib/tx_rollup.mli
    
    Changes not staged for commit:
    	modified:   flextesa/src/doc/mini-net.md
    	modified:   flextesa/src/lib/interactive_mini_network.ml

--- src/lib/tx_rollup.ml
+++ src/lib/tx_rollup.ml
@@ -1,27 +1,206 @@
 open Internal_pervasives
 
+module Account = struct
+  type t =
+    {name: string; operation_hash: string; address: string; out: string list}
+
+  let make ~name ~operation_hash ~address ~out : t =
+    {name; operation_hash; address; out}
+
+  (*  TODO this is wonky but it works. Maybe there is a better way.*)
+  let parse_origination ~lines =
+    let rec prefix_from_list ~prefix = function
+      | [] -> None
+      | x :: xs ->
+          if not (String.is_prefix x ~prefix) then prefix_from_list ~prefix xs
+          else
+            Some
+              (String.lstrip
+                 (String.chop_prefix x ~prefix |> Option.value ~default:x) )
+    in
+    let l = List.map lines ~f:String.lstrip in
+    Option.(
+      prefix_from_list ~prefix:"Operation hash is" l
+      >>= fun op ->
+      String.chop_prefix ~prefix:"'" op
+      >>= fun suf ->
+      String.chop_suffix ~suffix:"'" suf
+      >>= fun operation_hash ->
+      prefix_from_list ~prefix:"Originated tx rollup:" l
+      >>= fun address ->
+      prefix_from_list ~prefix:"Transaction rollup memorized as" l
+      >>= fun name -> return (make ~name ~operation_hash ~address ~out:lines))
+
+  let originate state ~name ~client ~acc =
+    Tezos_client.successful_client_cmd state ~client
+      ["originate"; "tx"; "rollup"; name; "from"; acc; "--burn-cap"; "15"]
+
+  let confirm state ~client ?(confirmations = 0) ~operation_hash () =
+    Tezos_client.successful_client_cmd state ~client
+      [ "wait"; "for"; operation_hash; "to"; "be"; "included"; "--confirmations"
+      ; Int.to_string confirmations ]
+
+  let originate_and_confirm state ~name ~client ~acc ?confirmations () =
+    originate state ~name ~client ~acc
+    >>= fun res ->
+    return (parse_origination ~lines:res#out)
+    >>= fun rollup ->
+    match rollup with
+    | None ->
+        System_error.fail_fatalf
+          "Tx_rollup.originate_and_confirm - failed to parse rollup."
+    | Some acc ->
+        confirm state ~client ?confirmations ~operation_hash:acc.operation_hash
+          ()
+        >>= fun conf -> return (acc, conf#out)
+end
+
+module Tx_node = struct
+  type mode = Observer | Accuser | Batcher | Maintenance | Operator | Custom
+
+  type t =
+    { id: string
+    ; port: int option
+    ; endpoint: int
+    ; protocol: Tezos_protocol.Protocol_kind.t
+    ; exec: Tezos_executable.t
+    ; client: Tezos_client.t
+    ; mode: mode
+    ; cors_origin: string option
+    ; account: Account.t
+    ; operation_signers: string list }
+
+  let make_path p ~config t =
+    Paths.root config // sprintf "tx-rollup-%s" t.id // p
+
+  let data_dir config t = make_path "data-dir" ~config t
+  let exec_path config t = make_path "exec" ~config t
+
+  open Tezos_executable.Make_cli
+
+  let mode_string = function
+    | Observer -> "observer"
+    | Accuser -> "accuser"
+    | Batcher -> "batcher"
+    | Maintenance -> "maintenance"
+    | Operator -> "operator"
+    | Custom -> "custom "
+
+  let signers ~operator ~batcher ?finalize_commitment ?remove_commitment
+      ?rejection ?dispatch_withdrawals () =
+    let or_op_key = function None -> operator | Some k -> k in
+    opt "operator" operator @ opt "batch-signer" batcher
+    @ opt "finalize-commitment-signer" (or_op_key finalize_commitment)
+    @ opt "remove-commitment-signer" (or_op_key remove_commitment)
+    @ opt "rejection-signer" (or_op_key rejection)
+    @ opt "dispatch-withdrawals-signer" (or_op_key dispatch_withdrawals)
+
+  let make ?id ?port ~endpoint ~protocol ~exec ~client ~mode ?cors_origin
+      ~account ~operation_signers () =
+    { id=
+        (fun s ->
+          sprintf "%s-%s-node-%s" account.Account.name (mode_string mode)
+            (Option.value s ~default:"000") )
+          id
+    ; port
+    ; endpoint
+    ; protocol
+    ; exec
+    ; client
+    ; mode
+    ; cors_origin
+    ; account
+    ; operation_signers }
+
+  let call state t command =
+    let client_dir = Tezos_client.base_dir ~state t.client in
+    let cors_origin =
+      match t.cors_origin with
+      | Some _ as s -> s
+      | None -> Environment_configuration.default_cors_origin state in
+    Tezos_executable.call state t.exec ~protocol_kind:t.protocol
+      ~path:(exec_path state t // sprintf "exec-toru-%s" t.id)
+      (* TODO try:  t.node.Tezos_node.rpc_port *)
+      ( opt "endpoint" (sprintf "http://localhost:%d" t.endpoint)
+      @ opt "base-dir" client_dir
+      (* @ opt "config-file" (config_file state t) *)
+      @ command
+      @ opt "data-dir" (data_dir state t)
+      @ Option.value_map cors_origin
+          ~f:(fun s ->
+            flag "cors-header=content-type" @ Fmt.kstr flag "cors-origin=%s" s
+            )
+          ~default:[]
+      @ flag "allow-deposit" @ t.operation_signers )
+
+  let init state t =
+    call state t
+      ( ["init"; mode_string t.mode; "config"; "for"; t.account.address]
+      @ flag "force" )
+
+  let run state t =
+    call state t ["run"; mode_string t.mode; "for"; t.account.address]
+
+  let start_script state t =
+    let open Genspio.EDSL in
+    check_sequence ~verbosity:`Output_all
+      [("init node", init state t); ("run node", run state t)]
+
+  let process state t script =
+    Running_processes.Process.genspio
+      (sprintf "%s-node-for-tx-rollup-%s" (mode_string t.mode) t.account.name)
+      (script state t)
+
+  (* TODO Maybe add signer key options too? *)
+
+  let cmdliner_term state ~extra_doc =
+    let open Cmdliner in
+    let open Term in
+    let docs =
+      Manpage_builder.section state ~rank:2 ~name:"TRANSACTIONAL ROLLUP NODE"
+    in
+    const (fun mode ->
+        match mode with
+        | "observer" -> Observer
+        | "accuser" -> Accuser
+        | "batcher" -> Batcher
+        | "maintenance" -> Maintenance
+        | "custom " -> Custom
+        | "operator" | _ -> Operator )
+    $ Arg.(
+        value
+          (opt string "operator"
+             (info ~docs ["tx-rollup-node-mode"]
+                ~doc:
+                  (sprintf
+                     "Set the rollup node's `mode` %s. Possible modes include: \
+                      operator, observer, accuser, batcher, maintenance and \
+                      custom."
+                     extra_doc ) ) ))
+end
+
 type t =
   { level: int
   ; name: string
   ; node: Tezos_executable.t
-  ; client: Tezos_executable.t }
-
-let originate state name client acc =
-  Tezos_client.successful_client_cmd state ~client
-    ["originate"; "tx"; "rollup"; name; "from"; acc; "--burn-cap"; "15"]
+  ; client: Tezos_executable.t
+  ; mode: Tx_node.mode }
 
 let executables ({client; node; _} : t) = [client; node]
 
+(* TODO
+   add option for conformations required
+   add option for mode
+*)
 let cmdliner_term base_state ~docs () =
   let open Cmdliner in
-  let open Cmdliner.Term in
-  let extra_doc =
-    Fmt.str " for the transactional rollups (requires --tx-rollup)" in
-  const (fun tx_rollup node client ->
+  let open Term in
+  let extra_doc = Fmt.str " for transactional rollups (requires --tx-rollup)" in
+  const (fun tx_rollup node client mode ->
       Option.map tx_rollup ~f:(fun (level, name) ->
           let txr_name =
             match name with None -> "flextesa-tx-rollup" | Some n -> n in
-          {level; name= txr_name; node; client} ) )
+          {level; name= txr_name; node; client; mode} ) )
   $ Arg.(
       value
         (opt
@@ -32,3 +211,4 @@ let cmdliner_term base_state ~docs () =
               ~docv:"LEVEL:TX-ROLLUP-NAME" ) ))
   $ Tezos_executable.cli_term ~extra_doc base_state `Tx_rollup_node "tezos"
   $ Tezos_executable.cli_term ~extra_doc base_state `Tx_rollup_client "tezos"
+  $ Tx_node.cmdliner_term ~extra_doc base_state
--- src/lib/tx_rollup.mli
+++ src/lib/tx_rollup.mli
@@ -1,27 +1,92 @@
 open Internal_pervasives
 
+module Account : sig
+  type t =
+    {name: string; operation_hash: string; address: string; out: string list}
+
+  val originate_and_confirm :
+       < application_name: string
+       ; console: Console.t
+       ; env_config: Environment_configuration.t
+       ; paths: Paths.t
+       ; runner: Running_processes.State.t
+       ; .. >
+    -> name:string
+    -> client:Tezos_client.t
+    -> acc:string
+    -> ?confirmations:int
+    -> unit
+    -> ( t * string list
+       , [> `Process_error of Process_result.Error.error
+         | `System_error of [`Fatal] * System_error.static ] )
+       Asynchronous_result.t
+  (** [originate_and_confirm state name client acc] is a call to tezo-client [client] call to
+      originate transactional rollup [name]. [acc] is the gass account. *)
+end
+
+module Tx_node : sig
+  type mode = Observer | Accuser | Batcher | Maintenance | Operator | Custom
+
+  type t =
+    { id: string
+    ; port: int option
+    ; endpoint: int
+    ; protocol: Tezos_protocol.Protocol_kind.t
+    ; exec: Tezos_executable.t
+    ; client: Tezos_client.t
+    ; mode: mode
+    ; cors_origin: string option
+    ; account: Account.t
+    ; operation_signers: string list }
+
+  val signers :
+       operator:string
+    -> batcher:string
+    -> ?finalize_commitment:string
+    -> ?remove_commitment:string
+    -> ?rejection:string
+    -> ?dispatch_withdrawals:string
+    -> unit
+    -> string list
+  (** [signers] is a list of operation signers for the specific rollup operations. *)
+
+  val make :
+       ?id:string
+    -> ?port:int
+    -> endpoint:int
+    -> protocol:Tezos_protocol.Protocol_kind.t
+    -> exec:Tezos_executable.t
+    -> client:Tezos_client.t
+    -> mode:mode
+    -> ?cors_origin:string
+    -> account:Account.t
+    -> operation_signers:string list
+    -> unit
+    -> t
+  (** [make] is a type for builing a tx_rollup_node command. *)
+
+  val start_script :
+       < env_config: Environment_configuration.t ; paths: Paths.t ; .. >
+    -> t
+    -> unit Genspio.Language.t
+  (** [start script] runs the tx_rollup_node commans init and run accourning to [t]. *)
+
+  val process :
+    'a -> t -> ('a -> t -> 'b Genspio.Language.t) -> Running_processes.Process.t
+
+  val cmdliner_term :
+       < manpager: Manpage_builder.State.t ; .. >
+    -> extra_doc:string
+    -> mode Cmdliner.Term.t
+  (** A cmdliner term for the tx_rollup_node "mode" option. *)
+end
+
 type t =
   { level: int
   ; name: string
   ; node: Tezos_executable.t
-  ; client: Tezos_executable.t }
-
-val originate :
-     < application_name: string
-     ; console: Console.t
-     ; env_config: Environment_configuration.t
-     ; paths: Paths.t
-     ; runner: Running_processes.State.t
-     ; .. >
-  -> string
-  -> Tezos_client.t
-  -> string
-  -> ( < err: string list ; out: string list ; status: Unix.process_status >
-     , [> `Process_error of Process_result.Error.error
-       | `System_error of [`Fatal] * System_error.static ] )
-     Asynchronous_result.t
-(** [orginate state name client acc ] is a tezos [client] call to orginate a Transactional Rollup
-     from account [acc]. The rollup will be geven the [name] provieded *)
+  ; client: Tezos_executable.t
+  ; mode: Tx_node.mode }
 
 val executables : t -> Tezos_executable.t list
 (** List of executables for the Transactional Rollup *)
@@ -31,4 +96,4 @@ val cmdliner_term :
   -> docs:string
   -> unit
   -> t option Cmdliner.Term.t
-(** Command line option which starts a transactional rollup and with the rollup node and client. *)
+(** List of executables for the Transactional Rollup *)
