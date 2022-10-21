de3a30adfda5d17330251f2b71e4cf4cb51558e0
Sebastien Mondet <seb@mondet.org>
2022-10-21 16:00:30 -0400

    Fix the `daem_n2a` test

--- src/scripts/full-sandbox-tests.sh
+++ src/scripts/full-sandbox-tests.sh
@@ -95,8 +95,8 @@ daem_n2a () {
 	   --protocol-kind "$before_alpha" \
            --next-protocol-kind Alpha \
 	   --second-baker octez-baker-alpha \
-	   --extra-dummy-proposals-batch-size 2 \
-	   --extra-dummy-proposals-batch-levels 3,5 \
+	   --extra-dummy-proposals-batch-size 1 \
+	   --extra-dummy-proposals-batch-levels 3 \
 	   --size 2 \
 	   --number-of-b 2 \
 	   --time-betw 3 \
