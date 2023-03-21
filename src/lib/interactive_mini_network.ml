ebb1a6ac09fd2c3e29c9cdaf89e3b2392c94214d
Phil Saxton <phillipsaxton@gmail.com>
2023-03-21 13:49:32 -0400

    Smart_rollup: Make echo contract conditional.
    Flextesa will no longer originate the contract if it is not using the
    default "echo kernel."

--- src/lib/interactive_mini_network.ml
+++ src/lib/interactive_mini_network.ml
@@ -466,56 +466,72 @@ let run state ~protocol ~size ~base_port ~clear_root ~no_daemons_for ?hard_fork
   match soru with
   | None -> return ()
   | Some soru -> (
-      (List.hd keys_and_daemons |> function
-       | None -> return ()
-       | Some (_, _, client, _, _) ->
-           (* Inicialze operator keys. *)
-           let op_acc = Tezos_protocol.soru_node_operator protocol in
-           let op_keys =
-             let name, priv =
-               Tezos_protocol.Account.(name op_acc, private_key op_acc)
-             in
-             Tezos_client.Keyed.make client ~key_name:name ~secret_key:priv
-           in
-           Tezos_client.Keyed.initialize state op_keys >>= fun _ ->
-           (* Originate SORU.*)
-           Smart_rollup.originate_and_confirm state ~client ~kernel:soru.kernel
-             ~account:op_keys.key_name ~confirmations:1 ()
-           >>= fun (origination_res, _confirmation_res) ->
-           (* Configure SORU node. *)
-           let soru_node =
-             let port = Test_scenario.Unix_port.(next_port nodes) in
-             Smart_rollup.Node.make_config ~mode:soru.node_mode
-               ~soru_addr:origination_res.address
-               ~operator_addr:op_keys.key_name (*  TODO dbg check key_name*)
-               ~rpc_port:port ~endpoint:base_port ~protocol:protocol.kind
-               ~exec:soru.node ~client ()
-           in
-           (* Start SORU node. *)
-           Running_processes.start state
-             Smart_rollup.Node.(start state soru_node)
-           >>= fun _ ->
-           (* Originate echo smart contract for default echo kernel. *)
-           Smart_rollup.Echo_contract.publish state ~client
-             ~account:op_keys.key_name
-           >>= fun echo_contract ->
-           (* Print SORU info. *)
-           Console.say state
-             EF.(
-               desc_list
-                 (haf "Smart Optimistic Rollup (soru) is ready:")
-                 [
-                   desc (af "Address:") (af "`%s`" origination_res.address);
-                   desc
-                     (af "Smart Rollup Node RPC port:")
-                     (af "`%d`"
-                        (Option.value soru_node.rpc_port
-                           ~default:9999 (*  TODO double check default *)));
-                   desc
-                     (af "Node Operator address")
-                     (af "`%s`" (Tezos_protocol.Account.pubkey_hash op_acc));
-                   desc (af "Echo contract address") (af "`%s`" echo_contract);
-                 ]))
+      begin
+        List.hd keys_and_daemons |> function
+        | None -> return ()
+        | Some (_, _, client, _, _) ->
+            (* Inicialze operator keys. *)
+            let op_acc = Tezos_protocol.soru_node_operator protocol in
+            let op_keys =
+              let name, priv =
+                Tezos_protocol.Account.(name op_acc, private_key op_acc)
+              in
+              Tezos_client.Keyed.make client ~key_name:name ~secret_key:priv
+            in
+            Tezos_client.Keyed.initialize state op_keys >>= fun _ ->
+            (* Originate SORU.*)
+            Smart_rollup.originate_and_confirm state ~client ~kernel:soru.kernel
+              ~account:op_keys.key_name ~confirmations:1 ()
+            >>= fun (origination_res, _confirmation_res) ->
+            (* Configure SORU node. *)
+            let soru_node =
+              let port = Test_scenario.Unix_port.(next_port nodes) in
+              Smart_rollup.Node.make_config ~mode:soru.node_mode
+                ~soru_addr:origination_res.address
+                ~operator_addr:op_keys.key_name ~rpc_port:port
+                ~endpoint:base_port ~protocol:protocol.kind ~exec:soru.node
+                ~client ()
+            in
+            (* Start SORU node. *)
+            Running_processes.start state
+              Smart_rollup.Node.(start state soru_node)
+            >>= fun _ ->
+            (match soru.kernel with
+            | None ->
+                (* Originate echo smart contract for default soru kernel. *)
+                Smart_rollup.Echo_contract.publish state ~client
+                  ~account:op_keys.key_name
+                >>= fun echo_contract ->
+                EF.(
+                  return
+                    [
+                      desc
+                        (af "Echo contract address")
+                        (af "`%s`" echo_contract);
+                    ])
+            | Some _ -> return [])
+            >>= fun echo_contract_addr ->
+            (* Print SORU info. *)
+            Console.say state
+              EF.(
+                desc_list
+                  (haf "Smart Optimistic Rollup (soru) is ready:")
+                  ([
+                     desc (af "Address:") (af "`%s`" origination_res.address);
+                     desc
+                       (af "Smart Rollup Node RPC port:")
+                       (af "`%d`"
+                          (Option.value_exn
+                             ?message:
+                               (Some
+                                  "Failed to get rpc port for Smart rollup \
+                                   node.") soru_node.rpc_port));
+                     desc
+                       (af "Node Operator address")
+                       (af "`%s`" (Tezos_protocol.Account.pubkey_hash op_acc));
+                   ]
+                  @ echo_contract_addr))
+      end
       >>= fun () ->
       let clients = List.map keys_and_daemons ~f:(fun (_, _, c, _, _) -> c) in
       Helpers.Shell_environement.(
